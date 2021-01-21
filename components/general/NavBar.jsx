51ae4343ccec26f0f2e6fe765b4abbc32268039d
Rupam Kairi <33358148+rupamkairi@users.noreply.github.com>
2021-01-21 10:55:28 +0530

    nav menu on mobile

--- components/general/NavBar.jsx
+++ components/general/NavBar.jsx
@@ -2,12 +2,12 @@ import React from "react";
 import Link from "next/link";
 
 export default function NavBar() {
-  const menuItems = ["Features", "Pricing", "Blogs", "Teams"];
+  const menuItems = ["Pricing", "Blogs"];
 
   return (
     <div className="container mx-auto py-4 flex justify-between items-center">
       <span className="text-4xl font-black">MyIO</span>
-      <div className="text-base font-medium invisible md:visible">
+      <div className="text-base font-medium md:visible">
         <Link href="/dashboard">Dashboard</Link>
         {menuItems.map((data, key) => (
           <a key={key} href="#" className="px-2">
