e4eb2b64c226f13c53d3f12b388c4a714ff86528
Rupam Kairi <33358148+rupamkairi@users.noreply.github.com>
2021-01-23 20:47:41 +0530

    sample database connection

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,6 +1,37 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+const MongoClient = require("mongodb").MongoClient;
 
-export default (req, res) => {
-  res.statusCode = 200;
-  res.json({ message: "Welcome to Myio API. All Endpoints are working fine." });
+export default async (req, res) => {
+  const client = new MongoClient(process.env.DB_URI);
+
+  try {
+    await client.connect();
+    const collection = client.db("myio_guests").collection("link_groups");
+    const query = {};
+    const groups = await collection.find(query);
+    // console.log(await groups.toArray());
+    // await groups.forEach((doc) => console.log(doc));
+
+    res.json({
+      message: "Welcome to Myio API. All Endpoints are working fine.",
+      secrets: {
+        database: {
+          // local: process.env.DB_URI,
+          // atlas: process.env.ATLAS_URI,
+          // client: client,
+          // database: database,
+          // collection: collection,
+          // query: query,
+          results: await groups.toArray(),
+        },
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.json({
+      message: "Some Error occured...",
+    });
+  } finally {
+    // await client.close();
+  }
 };
---
+++ pages/index.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+import Head from "next/head";
+import NavBar from "../components/general/NavBar";
+import Hero from "../components/home/Hero";
+
+export default function Home() {
+  const [state, setState] = useState();
+
+  useEffect(() => {
+    console.log(process.env.NEXT_PUBLIC_API_HOST);
+    fetch(process.env.NEXT_PUBLIC_API_HOST)
+      .then((res) => res.json())
+      .then((data) => {
+        setState(data);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <Head>
+        <title>MyIO</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <NavBar />
+      <Hero />
+      <pre className="text-xs">{JSON.stringify(state, null, 2)}</pre>
+    </div>
+  );
+}
