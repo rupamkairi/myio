e49bbc8b547730711baed9eda15df4fdd3b770d3
Rupam Kairi <33358148+rupamkairi@users.noreply.github.com>
2021-01-23 21:56:34 +0530

    ip whitelisting issue

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,42 +1,42 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
 export default async (req, res) => {
-  // const client = new MongoClient(process.env.DB_URI);
+  const client = new MongoClient(process.env.DB_URI);
 
   try {
-    // await client.connect();
-    // // console.log(client.isConnected());
-    // const collection = client.db("myio_guests").collection("link_groups");
-    // // console.log(collection.namespace);
-    // const query = {};
-    // const groups = await collection.find(query);
-    // console.log(await groups.toArray());
-    // await groups.forEach((doc) => console.log(doc));
+    await client.connect();
+    // console.log(client.isConnected());
+    const collection = client.db("myio_guests").collection("link_groups");
+    // console.log(collection.namespace);
+    const query = {};
+    const groups = await collection.find(query);
+    console.log(await groups.toArray());
+    await groups.forEach((doc) => console.log(doc));
 
     res.json({
       message: "Welcome to Myio API. All Endpoints are working fine.",
       secrets: {
-        // database: {
-        //   client_isConnected: client.isConnected(),
-        //   collection_namespace: collection.namespace,
-        //   // query: query,
-        //   results: await groups.toArray(),
-        // },
+        database: {
+          client_isConnected: client.isConnected(),
+          collection_namespace: collection.namespace,
+          // query: query,
+          results: await groups.toArray(),
+        },
       },
     });
   } catch (err) {
     res.json({
       message: "Some Error occured...",
       secrets: {
-        // database: {
-        //   client_isConnected: client.isConnected(),
-        //   collection_namespace: collection.namespace,
-        //   results: await groups.toArray(),
-        // },
+        database: {
+          client_isConnected: client.isConnected(),
+          collection_namespace: collection.namespace,
+          results: await groups.toArray(),
+        },
       },
     });
   } finally {
-    // await client.close();
+    await client.close();
   }
 };
