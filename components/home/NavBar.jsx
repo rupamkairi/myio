54e861154dfb5379930739f440eaa972b8e07cb5
Rupam Kairi <33358148+rupamkairi@users.noreply.github.com>
2021-01-27 21:33:14 +0530

    navbar try button fix

--- components/home/NavBar.jsx
+++ components/home/NavBar.jsx
@@ -6,14 +6,16 @@ export default function NavBar() {
 
   return (
     <div className="sticky top-0 bg-white">
-      <div className="container mx-auto py-4 px-4 flex justify-between items-center">
-        <Link href="/">
-          <span className="text-4xl font-black">MyIO</span>
-        </Link>
-        <div className="text-base font-medium md:visible">
-          <Link href="/dashboard">
-            <span className="btn btn-standard mx-2">Try for Once</span>
+      <div className="container mx-auto py-4 px-4 sm:grid sm:grid-cols-1 md:flex justify-between items-center">
+        <div className="w-full flex justify-between pb-8 md:pb-0">
+          <Link href="/">
+            <span className="text-4xl font-black">MyIO</span>
           </Link>
+          <Link href="/dashboard">
+            <span className="btn btn-standard md:mx-2">Try for Once</span>
+          </Link>
+        </div>
+        <div className="text-base font-medium">
           {menuItems.map((data, key) => (
             <Link key={key} href={`#${data}`}>
               <span className="mx-2">{data}</span>
