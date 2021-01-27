d4b4601961711a3874198cb9c031453240ecf35f
Rupam Kairi <33358148+rupamkairi@users.noreply.github.com>
2021-01-27 16:43:18 +0530

    change font

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,18 +26,12 @@ export default function Home() {
       <Head>
         <title>MyIO</title>
         <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@300;400;500;700&display=swap"
-          rel="stylesheet"
-        ></link>
       </Head>
       <NavBar />
       <Hero />
       <Features />
       <Pricing />
       <Footer />
-      {/* <pre className="text-xs">{JSON.stringify(state, null, 2)}</pre> */}
     </div>
   );
 }
