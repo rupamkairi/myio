901e6c2b4f71dd5390bd1847927afdefe5b5f367
Rupam Kairi <33358148+rupamkairi@users.noreply.github.com>
2021-01-28 22:05:56 +0530

    navbar layout and cursor fixes

--- components/home/NavBar.jsx
+++ components/home/NavBar.jsx
@@ -8,18 +8,20 @@ export default function NavBar() {
     <div className="sticky top-0 bg-white">
       <div className="container mx-auto py-4 px-4 sm:grid sm:grid-cols-1 md:flex justify-between items-center">
         <div className="w-full flex justify-between pb-8 md:pb-0">
-          <Link href="/">
-            <span className="text-4xl font-black">MyIO</span>
-          </Link>
-          <Link href="/dashboard">
-            <span className="btn btn-standard md:mx-2">Try for Once</span>
-          </Link>
+          <span className="text-4xl font-black">
+            <Link href="/">MyIO</Link>
+          </span>
+          <span className="btn btn-standard md:mx-2">
+            <Link href="/dashboard">Try for Once</Link>
+          </span>
         </div>
         <div className="text-base font-medium">
           {menuItems.map((data, key) => (
-            <Link key={key} href={`#${data}`}>
-              <span className="mx-2">{data}</span>
-            </Link>
+            <span className="mx-2">
+              <Link key={key} href={`#${data}`}>
+                {data}
+              </Link>
+            </span>
           ))}
         </div>
       </div>
