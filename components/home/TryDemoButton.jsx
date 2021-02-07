2c6f62370a9649e32d2cf9630edd1d1578042843
Rupam Kairi <33358148+rupamkairi@users.noreply.github.com>
2021-02-07 19:20:20 +0530

    Home Screen changes

--- components/home/TryDemoButton.jsx
+++ components/home/TryDemoButton.jsx
@@ -6,7 +6,7 @@ export default function TryDemoButton() {
     <div className="my-8">
       <Link href="/dashboard">
         <span className="text-2xl font-bold py-4 px-8 border rounded-md border-indigo-500 bg-indigo-500 text-white hover:bg-indigo-700 cursor-pointer">
-          Try for Once
+          Try Demo
         </span>
       </Link>
     </div>
