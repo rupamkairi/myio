dfc794a88f8ecf457bc67f983dec93772bc9be6f
Rupam Kairi <rupamkairi@hotmail.com>
2022-02-18 11:45:02 +0530

    Packages Updated

--- pages/api/index.js
+++ pages/api/index.js
@@ -1,44 +1,16 @@
-// const { View, FindBy, Add, AddLink } = require("./services/mongo.js");
-
 export default async (req, res) => {
   try {
-    // View("myio_guests", "link_groups");
-    // FindBy("myio_guests", "link_groups", { public: true });
-    // Add("myio_guests", "link_groups", {
-    //   group_title: "Rupam's Job Profile",
-    //   profile_name: "Job",
-    //   created_on: new Date().toDateString(),
-    //   public: false,
-    // });
-    // AddLink(
-    //   "myio_guests",
-    //   "link_groups",
-    //   { group_title: "Rupam's Job Profile" },
-    //   { platform: "github", username: "rupamkairi" }
-    // );
-    // AddLink(
-    //   "myio_guests",
-    //   "link_groups",
-    //   { group_title: "Rupam's Job Profile" },
-    //   { platform: "hackthebox", username: "gREp" }
-    // );
-
     res.json({
-      message: "Welcome to Myio API. All Endpoints are working fine.",
-      secrets: {
-        database: {
-          // db_uri: process.env.DB_URI,
-          // client_isConnected: client.isConnected()
-        },
+      message: "👍",
+      status: {
+        mongodb: {},
       },
     });
   } catch (err) {
     res.json({
-      message: "Some Error occured...",
-      secrets: {
-        database: {
-          // client_isConnected: client.isConnected()
-        },
+      message: "🌋",
+      status: {
+        mongodb: {},
       },
     });
   } finally {
---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import { Main } from "next/document";
+import Head from "next/head";
+import React from "react";
+
+export default function index() {
+  return (
+    <>
+      <Head>
+        <title>Document</title>
+      </Head>
+
+      <div>App</div>
+    </>
+  );
+}
